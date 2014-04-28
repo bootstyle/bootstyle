f1c811649198484572534201f0463284d35f36a2
Levi Thomason <me@levithomason.com>
2014-04-27 23:56:40 -0700

    adds buttons styles

--- js/main.js
+++ js/main.js
@@ -55,7 +55,10 @@ bootstyle.controller('bootstyleController', ['$scope', '$timeout', function($sco
         $scope.settings = {
             grid_container_class: 'container',
             RECOMPILE_LESS_DELAY: 300,
-            use_bootstrap_theme: false
+            additional_less: {
+                bootstrap_theme: false,
+                button_style: 'default'
+            }
         };
 
         $scope.less = {
@@ -110,74 +113,68 @@ bootstyle.controller('bootstyleController', ['$scope', '$timeout', function($sco
 
     $scope.recompileLESS = function() {
 
-        // After angular has digested and applied scope ($timeout) re-grab all the links and refresh
-        $timeout(function() {
+        /*
+         START COPY from LESS browser.js v1.7.0 ln 633, or in less.js 1.7.0 ln 8171
+         This section of less.js could be wrapped as a method, less.loadLinks().
+         TODO: Lots of ppl want the ability to dynamically add less files, make a PR?
+         */
+        var typePattern = /^text\/(x-)?less$/;
 
-            /*
-             START COPY
-             Copied from LESS browser.js v1.7.0 ln 633, or in less.js 1.7.0 ln 8171
-             This section of less.js could be wrapped as a method, less.loadLinks().
-             TODO: Lots of ppl want the ability to dynamically add less files, make a PR
-             */
-            var typePattern = /^text\/(x-)?less$/;
+        var links = document.getElementsByTagName('link');
 
-            var links = document.getElementsByTagName('link');
+        less.sheets = [];
 
-            less.sheets = [];
-
-            for (var i = 0; i < links.length; i++) {
-                if (links[i].rel === 'stylesheet/less' || (links[i].rel.match(/stylesheet/) &&
-                    (links[i].type.match(typePattern)))) {
-                    less.sheets.push(links[i]);
-                }
+        for (var i = 0; i < links.length; i++) {
+            if (links[i].rel === 'stylesheet/less' || (links[i].rel.match(/stylesheet/) &&
+                (links[i].type.match(typePattern)))) {
+                less.sheets.push(links[i]);
             }
-            // END COPY
+        }
+        // END COPY
 
-            var updatedVars = {
-                // colors
-                '@body-bg': $scope.less.body_bg,
+        var updatedVars = {
+            // colors
+            '@body-bg': $scope.less.body_bg,
 
-                '@brand-primary': $scope.less.brand.primary,
-                '@brand-success': $scope.less.brand.success,
-                '@brand-info': $scope.less.brand.info,
-                '@brand-warning': $scope.less.brand.warning,
-                '@brand-danger': $scope.less.brand.danger,
+            '@brand-primary': $scope.less.brand.primary,
+            '@brand-success': $scope.less.brand.success,
+            '@brand-info': $scope.less.brand.info,
+            '@brand-warning': $scope.less.brand.warning,
+            '@brand-danger': $scope.less.brand.danger,
 
-                // border radius
-                '@border-radius-base': $scope.less.border_radius_base + 'px',
-                '@border-radius-large': Math.floor($scope.less.border_radius_base * 1.5) + 'px',
-                '@border-radius-small': Math.floor($scope.less.border_radius_base * 0.5) + 'px',
+            // border radius
+            '@border-radius-base': $scope.less.border_radius_base + 'px',
+            '@border-radius-large': Math.floor($scope.less.border_radius_base * 1.5) + 'px',
+            '@border-radius-small': Math.floor($scope.less.border_radius_base * 0.5) + 'px',
 
-                // font
-                '@font-size-base': $scope.less.font_size_base + 'px',
-                '@font-family-base': $scope.less.font_family_base.preview || $scope.less.font_family_base.style,
-                '@headings-font-family': $scope.less.headings_font_family.preview || $scope.less.headings_font_family.style,
+            // font
+            '@font-size-base': $scope.less.font_size_base + 'px',
+            '@font-family-base': $scope.less.font_family_base.preview || $scope.less.font_family_base.style,
+            '@headings-font-family': $scope.less.headings_font_family.preview || $scope.less.headings_font_family.style,
 
-                // padding
-                '@padding-base-vertical': Math.floor($scope.less.padding.master * 0.6) + 'px',
-                '@padding-base-horizontal': Math.floor($scope.less.padding.master * 1.2) + 'px',
+            // padding
+            '@padding-base-vertical': Math.floor($scope.less.padding.master * 0.6) + 'px',
+            '@padding-base-horizontal': Math.floor($scope.less.padding.master * 1.2) + 'px',
 
-                '@padding-large-vertical': Math.floor($scope.less.padding.master * 1) + 'px',
-                '@padding-large-horizontal': Math.floor($scope.less.padding.master * 1.6) + 'px',
+            '@padding-large-vertical': Math.floor($scope.less.padding.master * 1) + 'px',
+            '@padding-large-horizontal': Math.floor($scope.less.padding.master * 1.6) + 'px',
 
-                '@padding-small-vertical': Math.floor($scope.less.padding.master * 0.5) + 'px',
-                '@padding-small-horizontal': Math.floor($scope.less.padding.master * 1) + 'px',
+            '@padding-small-vertical': Math.floor($scope.less.padding.master * 0.5) + 'px',
+            '@padding-small-horizontal': Math.floor($scope.less.padding.master * 1) + 'px',
 
-                '@padding-xs-vertical': Math.floor($scope.less.padding.master * 0.1) + 'px',
-                '@padding-xs-horizontal': Math.floor($scope.less.padding.master * 0.5) + 'px'
-            };
+            '@padding-xs-vertical': Math.floor($scope.less.padding.master * 0.1) + 'px',
+            '@padding-xs-horizontal': Math.floor($scope.less.padding.master * 0.5) + 'px'
+        };
 
-            less.refresh(true, updatedVars);
+        less.refresh(true, updatedVars);
 
-        });
     };
 
     /**
      Watches
      Any scope properties which require less to be recompiled are watched for changes
      */
-    $scope.$watch('[less, settings.use_bootstrap_theme]', function(newValue, oldValue) {
-
+    $scope.$watch('[less, settings.additional_less]', function(newValue, oldValue) {
         $scope.last_LESS_edit = Date.now();
         $scope.timerRecompileLESS();
 
