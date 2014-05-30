7fa0e7962da8570816e7dfa37d1eab9070f98ff1
Levi Thomason <me@levithomason.com>
2014-05-29 23:00:23 -0700

    IIFE sandbox for CupOf, totally side tracked here!

--- js/foo.js
+++ js/foo.js
@@ -1,40 +1,81 @@
-window.CupOf = (function() {
+(function() {
     'use strict';
 
-    // constructor
+
+    /**
+     * SETUP
+     * Includes private library code only available here in the IIFE's scope
+     */
+
+    // Attach us to the window
+    window.CupOf = CupOf;
+
+    // Internal reference object
+    // We can hide properties and methods here.
+    // We can also hold state from instance to instance
+    // Example, to issue an instance ID to new instances of CupOf
+    var cup_of = {
+        cups: [],
+        last_id: 0,
+        next_id: function() {
+            return cup_of.last_id += 1;
+        }
+    };
+
+    var iife_property = "I am a public property";
+
+    var iife_method = function() {
+        return "I am a public method";
+    };
+
+
+    /**
+     * LIBRARY CONSTRUCTOR
+     */
+
     function CupOf(contents) {
 
-        this.contents = contents;
+        if (contents) {
+            this.instance_id = cup_of.next_id();
+            this.contents = contents;
+            cup_of.cups.push(this);
+        } else {
+            throw 'CupOf needs some contents: CupOf("contents")';
+        }
 
-        // private constructor bits
+        // PRIVATE CONSTRUCTOR CODE
+        // Only available here in the Constructor's scope
+
         var private_property = 'this is a private property';
+
         var private_method = function() {
             return "I am a private method";
         };
     }
 
-    // private library bits
-    var public_property = "I am a public property";
-    var private_method = function() {
+
+    // Perhaps more private IIFE scoped code here
+
+    var another_iife_property = "I am a public property";
+
+    var another_iife_method = function() {
         return "I am a public method";
     };
 
 
-    // PUBLIC
-
-    // Mutable
-    // available without instantiation
-    CupOf.help = "Try: new CupOf(<your favorite drink>)";
-
-    // Immutable
-    // only available on instantiation
-    CupOf.prototype.pour = function() {
-        if (CupOf.contents) {
-            return 'Pouring a tasty coup of ' + CupOf.contents;
-        } else {
-            return 'Give me some contents first: CupOf.contents = "foo"';
-        }
+    CupOf.list_cups = function() {
+        return cup_of.cups;
     };
 
-    return CupOf;
+
+    /**
+     * PUBLIC OBJECT INSTANCE CODE
+     */
+
+    CupOf.prototype.contents = "They're in an object inaccessible to you!  Try pouring them out?";
+
+    CupOf.prototype.pour = function() {
+        return 'Pouring a big fat tasty cup of ' + cup_of.contents;
+    };
+
 }());
