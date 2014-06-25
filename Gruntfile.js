5e0bd6b657110ece2f17cf2c20f158198ce452c3
Levi Thomason <me@levithomason.com>
2014-06-25 12:51:32 -0700

    grunt browserify works

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,79 +2,89 @@ module.exports = function(grunt) {
 
     // configure the tasks
     grunt.initConfig({
-            pkg: grunt.file.readJSON('package.json'),
-            connect: {
+        pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            options: {
+                base: '.',
+            },
+            dev: {
                 options: {
-                    base: '.',
-                },
-                dev: {
-                    options: {
-                        port: 35729,
-                        protocol: 'http',
-                        hostname: '*',
-                        keepalive: true,
-                        debug: true,
-//                        livereload: true,
-                        open: false,
-                        useAvailablePort: false,
-                        onCreateServer: function(server, connect, options) {},
-                        middleware: function(connect, options, middlewares) {
+                    port: 35729,
+                    protocol: 'http',
+                    hostname: '*',
+                    keepalive: true,
+                    debug: true,
+                    //livereload: true,
+                    open: false,
+                    useAvailablePort: false,
+                    onCreateServer: function(server, connect, options) {
+                    },
+                    middleware: function(connect, options, middlewares) {
 
-                            // Must be the first middle-ware in the array, per the docs
-                            middlewares.push(function(connect) {
-                                require('connect-livereload')({
-                                    port: 35729
-                                });
+                        // Must be the first middle-ware in the array, per the docs
+                        middlewares.push(function(connect) {
+                            require('connect-livereload')({
+                                port: 35729
                             });
+                        });
 
-//                            middlewares.push(function(req, res, next) {
-//                                if (req.url !== '/') return next();
-//
-//                                res.end('Middleware working on "/"' + options.port);
-//                            });
-
-                            return middlewares;
-                        }
+                        return middlewares;
                     }
                 }
+            }
+        },
+        copy: {
+            build: {
+                cwd: 'node_modules',
+                src: [ '*/{lib,bin}/*.js' ],
+                dest: 'build',
+                flatten: true,
+                expand: true
             },
-            copy: {
-                build: {
-                    cwd: 'node_modules',
-                    src: [ '*/{lib,bin}/*.js' ],
-                    dest: 'build',
-                    flatten: true,
-                    expand: true
-                },
+        },
+        clean: {
+            build: {
+                src: [ 'build' ]
             },
-            clean: {
-                build: {
-                    src: [ 'build' ]
-                },
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
-            uglify: {
-                options: {
-                    banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-                },
-                build: {
-                    src: [ 'node_modules/*/{lib,bin}/*.js' ],
-                    dest: 'build/<%= pkg.name %>.min.js'
-                }
+            build: {
+                src: [ 'node_modules/*/{lib,bin}/*.js' ],
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        },
+        watch: {
+            files: ['js/*.js', 'js/vendor/*.js'],
+            tasks: ['jshint'],
+            options: {
+                cwd: '.',
+                atBegin: false,
+                spawn: false,
+                event: ['all'],
+                reload: true,
+                livereload: 35729,
+                livereloadOnError: true,
             },
-            watch: {
-                files: ['js/*.js', 'js/vendor/*.js'],
-                tasks: ['jshint'],
-                options: {
-                    cwd: '.',
-                    atBegin: false,
-                    spawn: false,
-                    event: ['all'],
-                    reload: true,
-                    livereload: 35729,
-                    livereloadOnError: true,
-                },
+        },
+        browserifyBower: {
+            options: {
+                // Task-specific options go here.
             },
-        });
+            your_target: {
+                // Target-specific file lists and/or options go here.
+            },
+        },
+        browserify: {
+            bundleOptions: {
+                debug: true
+            },
+            src: 'main.js',
+            dest: 'js/bundle.js'
+        },
+    });
 
     // load plugins
     grunt.loadNpmTasks('grunt-contrib-clean');
@@ -82,6 +92,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-browserify-bower');
 
     // define tasks
     // TODO: copy font awesome fonts to fonts folder
