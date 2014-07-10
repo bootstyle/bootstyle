19f0bfe6525b0c899f4ab32c3ecf03ba48094a6c
Levi Thomason <me@levithomason.com>
2014-07-10 00:58:36 -0700

    grunt is gone

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
     'use strict';
 
     var browserify = require('browserify');
-    var merge = require('merge-stream');
     var source = require('vinyl-source-stream'); // makes non-gulp pipelines (browserify, etc) gulp compatible: https://github.com/hughsk/vinyl-source-stream
     var del = require('del');
     var runSequence = require('run-sequence');
@@ -14,16 +13,15 @@
     var flatten = require('gulp-flatten');
     var minifyCSS = require('gulp-minify-css');
     var uglify = require('gulp-uglify');
-    var rimraf = require('gulp-rimraf');
-    var connect = require('gulp-connect');
     var webserver = require('gulp-webserver');
     var watch = require('gulp-watch');
     var plumber = require('gulp-plumber'); // keep streaming on error (used by gulp-watch): https://github.com/floatdrop/gulp-plumber
     var streamify = require('gulp-streamify'); // wrap old gulp plugins to use streams: https://github.com/nfroidure/gulp-streamify
+    var open = require("gulp-open");
 
 
     var pkg = require('./package.json');
-    var paths = {
+    var path = {
         app: './app/',
         bower: './bower_components/',
         build: './build/',
@@ -39,36 +37,36 @@
      */
     // app
     gulp.task('clean-app-bower', function() {
-        del.sync([paths.app + '**/bower/**']);
+        del.sync([path.app + '**/bower/**']);
     });
 
     // build
     gulp.task('clean-build', function() {
-        del.sync([paths.build]);
+        del.sync([path.build]);
     });
 
     gulp.task('clean-build-css', function() {
-        del.sync([paths.build + '**/css/**']);
+        del.sync([path.build + '**/css/**']);
     });
 
     gulp.task('clean-build-fonts', function() {
-        del.sync([paths.build + '**/fonts/**']);
+        del.sync([path.build + '**/fonts/**']);
     });
 
     gulp.task('clean-build-js', function() {
-        del.sync([paths.build + '**/js/**']);
+        del.sync([path.build + '**/js/**']);
     });
 
     gulp.task('clean-build-less', function() {
-        del.sync([paths.build + '**/less/**']);
+        del.sync([path.build + '**/less/**']);
     });
 
     gulp.task('clean-build-partials', function() {
-        del.sync([paths.build + '**/partials/**']);
+        del.sync([path.build + '**/partials/**']);
     });
 
     gulp.task('clean-build-root', function() {
-        del.sync([paths.build + '**/*.*']);
+        del.sync([path.build + '**/*.*']);
     });
 
 
@@ -84,22 +82,22 @@
         gulpBowerFiles()
             .pipe(cssFilter)
             .pipe(flatten())
-            .pipe(gulp.dest(paths.app + 'css/bower'))
+            .pipe(gulp.dest(path.app + 'css/bower'))
             .pipe(cssFilter.restore())
 
             .pipe(fontFilter)
             .pipe(flatten())
-            .pipe(gulp.dest(paths.app + 'fonts/bower'))
+            .pipe(gulp.dest(path.app + 'fonts/bower'))
             .pipe(fontFilter.restore())
 
             .pipe(jsFilter)
             .pipe(flatten())
-            .pipe(gulp.dest(paths.app + 'js/bower'))
+            .pipe(gulp.dest(path.app + 'js/bower'))
             .pipe(jsFilter.restore())
 
             .pipe(bootstrapLessFilter)
             .pipe(flatten())
-            .pipe(gulp.dest(paths.app + 'less/bower/bootstrap'));
+            .pipe(gulp.dest(path.app + 'less/bower/bootstrap'));
     });
 
 
@@ -120,16 +118,16 @@
     });
 
     gulp.task('build-css', ['clean-build-css'], function() {
-        return gulp.src(paths.app + paths.css)
+        return gulp.src(path.app + path.css)
             .pipe(watch())
             .pipe(plumber())
             .pipe(minifyCSS({ keepSpecialComments: 0 }))
             //.pipe(concat(pkg.name.toLowerCase() + '.min.css'))
-            .pipe(gulp.dest(paths.build + 'css'));
+            .pipe(gulp.dest(path.build + 'css'));
     });
 
     gulp.task('build-js', ['clean-build-js'], function() {
-        var b = browserify(paths.app + 'js/app.js');
+        var b = browserify(path.app + 'js/app.js');
         var bundleStream = b.bundle();
         var gulpBrowserify = bundleStream.pipe(source('app.js'));
 
@@ -137,35 +135,35 @@
             .pipe(watch())
             .pipe(plumber())
             .pipe(streamify(uglify({ mangle: true })))
-            .pipe(gulp.dest(paths.build + 'js'));
+            .pipe(gulp.dest(path.build + 'js'));
     });
 
     gulp.task('build-fonts', ['clean-build-fonts'], function() {
-        return gulp.src(paths.app + paths.fonts)
+        return gulp.src(path.app + path.fonts)
             .pipe(watch())
             .pipe(plumber())
-            .pipe(gulp.dest(paths.build + 'fonts'));
+            .pipe(gulp.dest(path.build + 'fonts'));
     });
 
     gulp.task('build-less', ['clean-build-less'], function() {
-        return gulp.src(paths.app + paths.less)
+        return gulp.src(path.app + path.less)
             .pipe(watch())
             .pipe(plumber())
-            .pipe(gulp.dest(paths.build + 'less'));
+            .pipe(gulp.dest(path.build + 'less'));
     });
 
     gulp.task('build-partials', ['clean-build-partials'], function() {
-        return gulp.src(paths.app + paths.partials)
+        return gulp.src(path.app + path.partials)
             .pipe(watch())
             .pipe(plumber())
-            .pipe(gulp.dest(paths.build + 'partials'));
+            .pipe(gulp.dest(path.build + 'partials'));
     });
 
     gulp.task('build-root', ['clean-build-root'], function() {
-        return gulp.src(paths.app + '*.*')
+        return gulp.src(path.app + '*.*')
             .pipe(watch())
             .pipe(plumber())
-            .pipe(gulp.dest(paths.build));
+            .pipe(gulp.dest(path.build));
     });
 
 
@@ -187,6 +185,13 @@
             }));
     });
 
+    /**
+     Open
+     */
+    gulp.task('open', function() {
+        gulp.src(pkg.main)
+            .pipe(open("", { url: 'http://localhost:8000' }));
+    });
 
     /**
      Default
@@ -194,7 +199,8 @@
     gulp.task('default', function() {
         runSequence(
             'build',
-            'connect-dev'
+            'connect-dev',
+            'open'
         )
     });
 
