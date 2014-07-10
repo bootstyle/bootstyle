33b0d7f9848f72f6d99c54d89be2b7e802d09979
Levi Thomason <me@levithomason.com>
2014-07-10 00:33:07 -0700

    gulp build, connect, livereload working

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,9 @@
 (function() {
     'use strict';
 
-    var config = require('./build.config.json');
-    var pkg = require('./package.json');
-
     var browserify = require('browserify');
     var merge = require('merge-stream');
-    var source = require('vinyl-source-stream');
+    var source = require('vinyl-source-stream'); // makes non-gulp pipelines (browserify, etc) gulp compatible: https://github.com/hughsk/vinyl-source-stream
     var del = require('del');
     var runSequence = require('run-sequence');
 
@@ -18,83 +15,93 @@
     var minifyCSS = require('gulp-minify-css');
     var uglify = require('gulp-uglify');
     var rimraf = require('gulp-rimraf');
-    var gutil = require('gulp-util');
+    var connect = require('gulp-connect');
+    var webserver = require('gulp-webserver');
+    var watch = require('gulp-watch');
+    var plumber = require('gulp-plumber'); // keep streaming on error (used by gulp-watch): https://github.com/floatdrop/gulp-plumber
+    var streamify = require('gulp-streamify'); // wrap old gulp plugins to use streams: https://github.com/nfroidure/gulp-streamify
 
+
+    var pkg = require('./package.json');
+    var paths = {
+        app: './app/',
+        bower: './bower_components/',
+        build: './build/',
+        css: 'css/**/*.css',
+        fonts: 'fonts/**/*.*',
+        js: 'js/**/*.js',
+        less: 'less/**/*.less',
+        partials: 'partials/**/*.html',
+    };
 
     /**
      Clean
      */
-
     // app
     gulp.task('clean-app-bower', function() {
-        del.sync([config.app_dir + '**/bower/**']);
+        del.sync([paths.app + '**/bower/**']);
     });
 
     // build
     gulp.task('clean-build', function() {
-        del.sync([config.build_dir]);
+        del.sync([paths.build]);
     });
 
     gulp.task('clean-build-css', function() {
-        del.sync([config.build_dir + '**/css/**']);
+        del.sync([paths.build + '**/css/**']);
     });
 
     gulp.task('clean-build-fonts', function() {
-        del.sync([config.build_dir + '**/fonts/**']);
+        del.sync([paths.build + '**/fonts/**']);
     });
 
     gulp.task('clean-build-js', function() {
-        del.sync([config.build_dir + '**/js/**']);
+        del.sync([paths.build + '**/js/**']);
     });
 
     gulp.task('clean-build-less', function() {
-        del.sync([config.build_dir + '**/less/**']);
+        del.sync([paths.build + '**/less/**']);
     });
 
     gulp.task('clean-build-partials', function() {
-        del.sync([config.build_dir + '**/partials/**']);
+        del.sync([paths.build + '**/partials/**']);
     });
 
     gulp.task('clean-build-root', function() {
-        del.sync([config.build_dir + '**/*.*']);
+        del.sync([paths.build + '**/*.*']);
     });
 
 
     /**
      Copy Bower Components
      */
-    gulp.task('copy-bower', ['clean-app-bower'], function(cb) {
+    gulp.task('copy-bower', ['clean-app-bower'], function() {
         var cssFilter = gulpFilter('**/*.css');
         var fontFilter = gulpFilter([ '**/*.otf', '**/*.eot', '**/*.svg', '**/*.ttf', '**/*.woff' ]);
         var jsFilter = gulpFilter('**/*.js');
         var bootstrapLessFilter = gulpFilter('bootstrap/**/*.less');
 
-        gulpBowerFiles()
+        return gulpBowerFiles()
+            .pipe(watch())
+            .pipe(plumber())
             .pipe(cssFilter)
             .pipe(flatten())
-            .pipe(gulp.dest(config.app_dir + 'css/bower'))
+            .pipe(gulp.dest(paths.app + 'css/bower'))
             .pipe(cssFilter.restore())
 
             .pipe(fontFilter)
             .pipe(flatten())
-            .pipe(gulp.dest(config.app_dir + 'fonts/bower'))
+            .pipe(gulp.dest(paths.app + 'fonts/bower'))
             .pipe(fontFilter.restore())
 
             .pipe(jsFilter)
             .pipe(flatten())
-            .pipe(gulp.dest(config.app_dir + 'js/bower'))
+            .pipe(gulp.dest(paths.app + 'js/bower'))
             .pipe(jsFilter.restore())
 
             .pipe(bootstrapLessFilter)
             .pipe(flatten())
-            .pipe(gulp.dest(config.app_dir + 'less/bower/bootstrap'));
-
-        console.error('\n    !!!! Fix gulpBowerFiles setTimeout Hack !!!!\n');
-        // TODO: fix this hack!  This task is not running synchronously, therefore build js tries to run before copy is done, is it a proper stream?
-        setTimeout(function() {
-            cb(null);
-        }, 1000);
-
+            .pipe(gulp.dest(paths.app + 'less/bower/bootstrap'));
     });
 
 
@@ -103,7 +110,6 @@
      */
     gulp.task('build', function() {
         runSequence(
-            'copy-bower',
             [
                 'build-css',
                 'build-fonts',
@@ -112,56 +118,75 @@
                 'build-partials',
                 'build-root'
             ]
-        );
+        )
     });
 
     gulp.task('build-css', ['clean-build-css'], function() {
-        return gulp.src(config.app_dir + 'css/**/*.css')
+        return gulp.src(paths.app + paths.css)
+            .pipe(watch())
+            .pipe(plumber())
             .pipe(minifyCSS({ keepSpecialComments: 0 }))
             //.pipe(concat(pkg.name.toLowerCase() + '.min.css'))
-            .pipe(gulp.dest(config.build_dir + 'css'));
+            .pipe(gulp.dest(paths.build + 'css'));
     });
 
     gulp.task('build-js', ['clean-build-js'], function() {
-        return browserify(config.app_dir + 'js/app.js')
-            .bundle()
-            .pipe(source('app.js'))
-            // Start piping stream to tasks!
-            .pipe(gulp.dest(config.build_dir + 'js'));
+        var b = browserify(paths.app + 'js/app.js');
+        var bundleStream = b.bundle();
+        var gulpBrowserify = bundleStream.pipe(source('app.js'));
+
+        return gulpBrowserify
+            .pipe(watch())
+            .pipe(plumber())
+            .pipe(streamify(uglify({ mangle: true })))
+            .pipe(gulp.dest(paths.build + 'js'));
     });
 
     gulp.task('build-fonts', ['clean-build-fonts'], function() {
-        return gulp.src(config.app_dir + 'fonts/**/*.*')
-            .pipe(gulp.dest(config.build_dir + 'fonts'));
+        return gulp.src(paths.app + paths.fonts)
+            .pipe(watch())
+            .pipe(plumber())
+            .pipe(gulp.dest(paths.build + 'fonts'));
     });
 
     gulp.task('build-less', ['clean-build-less'], function() {
-        return gulp.src(config.app_dir + 'less/**/*.*')
-            .pipe(gulp.dest(config.build_dir + 'less'));
+        return gulp.src(paths.app + paths.less)
+            .pipe(watch())
+            .pipe(plumber())
+            .pipe(gulp.dest(paths.build + 'less'));
     });
 
     gulp.task('build-partials', ['clean-build-partials'], function() {
-        return gulp.src(config.app_dir + 'partials/**/*.*')
-            .pipe(gulp.dest(config.build_dir + 'partials'));
+        return gulp.src(paths.app + paths.partials)
+            .pipe(watch())
+            .pipe(plumber())
+            .pipe(gulp.dest(paths.build + 'partials'));
     });
 
     gulp.task('build-root', ['clean-build-root'], function() {
-        return gulp.src(config.app_dir + '*.*')
-            .pipe(gulp.dest(config.build_dir));
+        return gulp.src(paths.app + '*.*')
+            .pipe(watch())
+            .pipe(plumber())
+            .pipe(gulp.dest(paths.build));
     });
 
 
     /**
-     Watch
+     Connect
      */
-    gulp.task('watch', function() {
-        gulp.watch(config.app_dir + 'css/**/*.css', ['build-css']);
-        gulp.watch(config.app_dir + 'fonts/**/*.fonts', ['build-fonts']);
-        gulp.watch(config.app_dir + 'js/**/*.js', ['build-js']);
-        gulp.watch(config.app_dir + 'less/**/*.less', ['build-less']);
-        gulp.watch(config.app_dir + 'partials/**/*.html', ['build-partials']);
-        gulp.watch(config.app_dir + '*.*', ['build-root']);
-        gulp.watch(config.bower_dir + '**', ['build']);
+    gulp.task('connect-dev', function() {
+        return gulp.src('build')
+            .pipe(webserver({
+                livereload: true,
+                fallback: 'index.html'
+            }));
+    });
+
+    gulp.task('connect-prod', function() {
+        return gulp.src('app')
+            .pipe(webserver({
+                port: 80
+            }));
     });
 
 
@@ -171,8 +196,8 @@
     gulp.task('default', function() {
         runSequence(
             'build',
-            'watch'
-        );
+            'connect-dev'
+        )
     });
 
 }());
